cbd9b7fad4c93449e9e88db9dcd6e48a9357af68
Moshiul Fahim <bookmyturfbd@gmail.com>
2026-06-05 12:22:23 +0600

    Update page.tsx for  SEO
    Aneek

--- src/app/[locale]/shop/checkout/page.tsx
+++ src/app/[locale]/shop/checkout/page.tsx
@@ -3,6 +3,10 @@ import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Checkout | BMT Shop',
+  robots: {
+    index: false,
+    follow: false,
+  },
 };
 
 export default function CheckoutPage() {
